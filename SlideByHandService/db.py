c9d1a43c26da85a7491ebc98cce7d69db554f755
liuw <1458476478@qq.com>
2022-01-08 12:25:04 +0800

    -- 上传 app 工程

---
+++ SlideByHandService/db.py
@@ -0,0 +1,35 @@
+
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, FLOAT
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class SlidePath(Base):
+    __tablename__ = "slide_path"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    end_x = Column(FLOAT)
+    end_y = Column(FLOAT)
+    path_json = Column(String)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+Base.metadata.create_all(engine)
+
+
+def get_session():
+    db = SessionLocal()
+    db.execute('pragma foreign_keys=on')
+    try:
+        yield db
+    finally:
+        db.close()
